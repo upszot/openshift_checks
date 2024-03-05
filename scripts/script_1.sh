ae013e57340f76f62769e92a283d3caae324c64b
upszot <upszot@gmail.com>
2024-03-05 15:26:04 -0300

    add generacion archivo en tmp..

--- scripts/script_1.sh
+++ scripts/script_1.sh
@@ -6,6 +6,5 @@ CLUSTER_URL=$3
 CLUSTER_PASSWORD=$4
 
 echo "script 1: "
-echo "Namespace: $NAMESPACE Cluster: $CLUSTER URL: $CLUSTER_URL -- Clave: $CLUSTER_PASSWORD"
-
+echo "Namespace: $NAMESPACE Cluster: $CLUSTER URL: $CLUSTER_URL -- Clave: $CLUSTER_PASSWORD" | tee -a /tmp/prueba_script_1.log
 
